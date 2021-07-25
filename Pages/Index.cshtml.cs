7fa003d765366cc3965c6436e5e4efca8bd20a1e
Sean Miller <raisingawesome@gmail.com>
2021-07-25 02:48:52 +0100

    updating

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using System.Net.Http;
 namespace ra_cam.Pages
 {
     public class IndexModel : PageModel
@@ -24,7 +24,48 @@ namespace ra_cam.Pages
         public IActionResult OnPostMine()
         {
             // request buffer in jobject
-            return new JsonResult(Request.Query["value"]);
+            return new JsonResult("hello");
+        }
+        public async Task<IActionResult> OnPostGetter()
+        {
+            // request buffer in jobject
+            string the_msg="nothing";
+            using (var httpClient = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://" + Request.Query["value"] +"/"+ Request.Query["the_type"]))
+                    {
+                        try {
+                            var response = await httpClient.SendAsync(request);
+                            var contents = await response.Content.ReadAsStringAsync();
+                            the_msg=contents.ToString();
+                        }
+                        catch (Exception e){
+                            the_msg="http://" + Request.Query["value"] +"/"+ Request.Query["the_type"];
+                        }                     
+                    }
+                }
+            return new JsonResult(the_msg);
+        }
+        public async Task<IActionResult> OnPostSetter()
+        {
+            // request buffer in jobject
+            string the_msg="nothing";
+            using (var httpClient = new HttpClient())
+                {
+                     using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://" + Request.Query["value"] +"/set"+ Request.Query["the_type"]))
+                    
+                    {
+                        try {
+                            var response = await httpClient.SendAsync(request);
+                            var contents = await response.Content.ReadAsStringAsync();
+                            the_msg=contents.ToString();
+                        }
+                        catch (Exception e){
+                            the_msg="http://" + Request.Query["value"] +"/set"+ Request.Query["the_type"];
+                        }                     
+                    }
+                }
+            return new JsonResult(the_msg);
         }
     }
 }
